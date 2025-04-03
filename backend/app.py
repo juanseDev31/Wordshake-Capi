d25b28a07ff35ea083ce2f4062804c28d0c17301
Juan Galeano <juansedev31@gmail.com>
2025-04-02 19:27:24 -0500

    update

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.users import users_bp  # Importar las rutas de usuarios
-from routes.auth import auth_bp  # Importar las rutas de autenticación
-from routes.check_word_espaniol import check_word_espaniol  # Importar las rutas de verificación de palabras
+from routes.users import users_bp  
+from routes.auth import auth_bp  
+from routes.check import check_word  
+
 app = Flask(__name__)
 CORS(app)
 
 # Registrar las rutas con un prefijo
 app.register_blueprint(users_bp, url_prefix='/api')
 app.register_blueprint(auth_bp, url_prefix='/api')
-app.register_blueprint(check_word_espaniol, url_prefix='/api') 
-
+app.register_blueprint(check_word, url_prefix='/api')  
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/routes/check.py
+++ backend/routes/check.py
@@ -1,23 +1,21 @@
 from flask import Blueprint, request, jsonify
 from spellchecker import SpellChecker
 
-check_word_espaniol = Blueprint('spell', __name__)
+check_word = Blueprint('check_word', __name__)
 
-# Aquí basicamente lo que hice fue crear un diccionario qeu contiene los idiomas y sus respectivos correctores ortográficos
-# de la librería spellchecker, para que luego al momento de hacer la petición se pueda elegir el idioma.
+# Aquí basicamente cree un diccioanrio para asi poder elegir si quiere espaniol o ingles como lenguaj e pa buscar la palabra
 spell_checkers = {
     "es": SpellChecker(language='es'),  
     "en": SpellChecker(language='en')   
 }
 
-# Esta funci;ón simplemnete va a calcular el porcentaje obtenido dependiendo la sletras que tenga la palabra
-# y el máximo de letras que puede tener una palabra, en este caso 10.
+# Calcula el score de la palabra basado en su longitud, de dos letras sera un punto y asi sucesivamnete hasta 10 letras
 def calcular_score(palabra):
     longitud = len(palabra)
     return min(longitud - 1, 10)  
 
-@check_word_espaniol.route('/check_word', methods=['POST'])
-def check_word():
+@check_word.route('/check', methods=['POST'])
+def check_word_route():  
     data = request.get_json()
     palabra = data.get("word")
     idioma = data.get("language", "es")  
@@ -28,7 +26,7 @@ def check_word():
     if idioma not in spell_checkers:
         return jsonify({"error": "Idioma no soportado"}), 400
 
-    # Seleccionar el corrector ortográfico del idioma elegido, si es español o si es inglés 
+    # Seleccionar el corrector ortográfico del idioma elegido, esto se hacea traves del diccionario que se creo antes
     spell = spell_checkers[idioma]
 
     if palabra in spell:
