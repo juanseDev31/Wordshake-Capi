9b08e27696fcd60a5ceabaa5ee3c4998afe5d4b4
Juan Galeano <juansedev31@gmail.com>
2025-04-02 19:38:47 -0500

    add the new api, check word

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.users import users_bp  # Importar las rutas de usuarios
 from routes.auth import auth_bp  # Importar las rutas de autenticación
+from routes.check import check_word  # Importar las rutas de verificación de palabras
 
 app = Flask(__name__)
 CORS(app)
@@ -9,7 +10,7 @@ CORS(app)
 # Registrar las rutas con un prefijo
 app.register_blueprint(users_bp, url_prefix='/api')
 app.register_blueprint(auth_bp, url_prefix='/api')
-
+app.register_blueprint(check_word, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
